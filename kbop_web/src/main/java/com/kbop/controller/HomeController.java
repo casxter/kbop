d8f5a63e37c316a0d388386090efddedb4b2fb23
TXML <casxter@qq.com>
2017-10-23 22:35:27 +0800

    HomeController.java

--- kbop_web/src/main/java/com/kbop/controller/HomeController.java
+++ kbop_web/src/main/java/com/kbop/controller/HomeController.java
@@ -1,15 +1,15 @@
 package com.kbop.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home() {
-        return "forward:page/home.html";
+        return "forward:/page/home.html";
     }
 
     @RequestMapping(value = "empty", method = RequestMethod.GET)
